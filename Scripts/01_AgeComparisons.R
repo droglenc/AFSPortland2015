c4627030ab5ea736e4edd11e84fe3aea5af3d935
droglenc <dogle@northland.edu>
2015-07-26 10:55:30 -0500

    Updated AgeComparisons

--- Scripts/01_AgeComparisons.R
+++ Scripts/01_AgeComparisons.R
@@ -1,22 +1,24 @@
 # AFS Portland 16-Aug-15
 
-library(FSA)                              # for ageBias(), agePrecision()
+library(FSA)                              # for headtail(), ageBias(), agePrecision()
+library(FSAdata)                          # for StripedBass4 data
 
-SB <- read.csv("data/StripedBass4.csv")   # appropriately set the working directory before this
+data(StripedBass4)
+SB <- StripedBass4
 str(SB)
+headtail(SB)
 
 ab <- ageBias(reader2~reader1,data=SB)
 
 summary(ab,what="table",flip.table=TRUE)
 summary(ab,what="symmetry")
-
 summary(ab,what="bias")
 
-plot(ab)                                              # Left
-plot(ab,diff=TRUE)                                    # Right
+plot(ab)                                                        # Left
+plot(ab,diff=TRUE,show.n=FALSE)                                 # Right
 
-plot(ab,diff=TRUE,show.range=TRUE)                    # Left
-plot(ab,diff=TRUE,show.pts=TRUE,transparency=1/25)    # Right
+plot(ab,diff=TRUE,show.n=FALSE,show.range=TRUE)                 # Left
+plot(ab,diff=TRUE,show.n=FALSE,show.pts=TRUE,transparency=1/25) # Right
 
 plot(ab,what="numbers",xlim=c(2,20),ylim=c(2,20))
 
@@ -30,8 +32,8 @@ summary(ap,what="detail")  # only some rows shown
 # ############################################################
 # This is a trick so that it appears that only some rows in
 # summary(ap,what="detail") are shown in the handout
-headtail(ap$detail)
+headtail(ap$detail,n=5)
 # ############################################################
 
 
-# Script created at 2015-07-23 19:38:21
+# Script created at 2015-07-26 09:31:40
